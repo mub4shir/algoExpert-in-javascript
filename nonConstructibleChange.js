16cf1e2e1325d536da2e8572080eabdd5129490d
mubashir <mubashir.ali@dror.co.in>
2022-08-24 01:49:01 +0530

    #5 done

---
+++ nonConstructibleChange.js
@@ -0,0 +1,13 @@
+// O(nlog(n)) time | O(1) space
+function nonConstructibleChange(coins) {
+    coins.sort((a, b) => a - b);
+    let currentChangeCreated = 0;
+    for (const coin of coins) {
+        if (coin > currentChangeCreated + 1) return currentChangeCreated + 1;
+        currentChangeCreated += coin;
+    }
+    return currentChangeCreated + 1
+}
+
+const coins = [5, 7, 1, 1, 2, 3, 22];
+console.log(nonConstructibleChange(coins))
